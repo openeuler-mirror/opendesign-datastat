383552f0cb299b286bcef9acc1d81358cdefa188
zjwmiao <1723168479@qq.com>
2025-05-26 20:04:38 +0800

    fix: 用户贡献页面，用户主页链接从接口获取

--- packages/gauss/src/views/person/index.vue
+++ packages/gauss/src/views/person/index.vue
@@ -15,6 +15,7 @@ import ContributionDynamic from './ContributionDynamic.vue';
 import DataShow from './DataShow.vue';
 import { debounce } from 'lodash-es';
 import { computed } from 'vue';
+import { request } from 'shared/plugins/axios';
 const useCommon = useCommonStore();
 const router = useRouter();
 const route = useRoute();
@@ -29,12 +30,19 @@ group.value = route.query.group as string;
 
 const userMap = ref<Map<string, string[]>>();
 
+const accountUrl = ref('');
+
 const accountHome = computed(() => {
   const accountName = sencondTitle.value;
+  let res: string;
   if (userMap.value?.get(accountName)?.includes('gitcode')) {
-    return `https://gitcode.com/${accountName}`
+    res = `https://gitcode.com/${accountName}`;
   }
-  return `https://gitee.com/${accountName}`
+  res = `https://gitee.com/${accountName}`;
+  if (accountUrl.value) {
+    res = accountUrl.value;
+  }
+  return res;
 });
 
 const getDrownData = () => {
@@ -44,7 +52,7 @@ const getDrownData = () => {
     // name: sigTitle.value,
   };
   queryUserList(query as any).then((res) => {
-    const data = (res?.data || {});
+    const data = res?.data || {};
     userMap.value = Object.entries(data as Record<string, string[]>).reduce((map, [key, val]) => map.set(key, val), new Map());
     drownData.value = [...userMap.value.keys()].sort((a: any, b: any) => a.localeCompare(b));
     getllData();
@@ -59,10 +67,10 @@ const contributeDetailCounts = ref({
   sig: 0,
 });
 
-const updateCounts = (arg: {user: string; timeRange: string }) => {
+const updateCounts = (arg: { user: string; timeRange: string }) => {
   queryUserContributeCounts({
     ...arg,
-    community: "opengauss",
+    community: 'opengauss',
   }).then((res) => {
     const { sig, pr, comment, issue } = res?.data || {};
     contributeDetailCounts.value.pr = pr || 0;
@@ -75,7 +83,16 @@ const updateCounts = (arg: {user: string; timeRange: string }) => {
 const anchorData = ['SIGContribution', 'DynamicContribute'];
 const clickDrownItem = (item: string) => {
   sencondTitle.value = item;
+  getUserAccountUrl();
   getllData();
+};
+
+const getUserAccountUrl = () => {
+  request.get('/stat/user/info', { params: { community: 'opengauss', user: sencondTitle.value } }).then((res) => {
+    if (res.data?.code === 1) {
+      accountUrl.value = res.data.data?.html_url ?? '';
+    }
+  });
 };
 
 const getllData = () => {
@@ -84,6 +101,7 @@ const getllData = () => {
   querySigInfoData();
 };
 onMounted(() => {
+  getUserAccountUrl();
   getDrownData();
   querySigInfoData();
 });
@@ -96,9 +114,7 @@ const searchInput = ref('');
 const reallData = ref([] as IObject[]);
 const querySearch = debounce(() => {
   if (searchInput.value !== '') {
-    const newList = drownData.value.filter((item: any) =>
-      item.toLowerCase().includes(searchInput.value)
-    );
+    const newList = drownData.value.filter((item: any) => item.toLowerCase().includes(searchInput.value));
     reallData.value = newList;
   } else {
     reallData.value = drownData.value;
@@ -123,20 +139,14 @@ const querySigInfoData = () => {
   queryUserOwnertype(params).then((res) => {
     sigInfo.value = res?.data || [];
     if (!sigInfo.value.length) return;
-    sigInfo.value.sort((a: any, b: any) =>
-      a['sig'].localeCompare(b['sig'], 'zh')
-    );
+    sigInfo.value.sort((a: any, b: any) => a['sig'].localeCompare(b['sig'], 'zh'));
     sigInfo.value.forEach((item: any, index: any) => {
       if (item.sig === 'TC') {
         sigInfo.value.unshift(sigInfo.value.splice(index, 1)[0]);
       }
     });
-    sigInfo.value.map((item: any) =>
-      item.type.sort((a: any, b: any) => b.length - a.length)
-    );
-    sigInfo.value.sort((a: any, b: any) =>
-      b['type'][0].localeCompare(a['type'][0], 'zh')
-    );
+    sigInfo.value.map((item: any) => item.type.sort((a: any, b: any) => b.length - a.length));
+    sigInfo.value.sort((a: any, b: any) => b['type'][0].localeCompare(a['type'][0], 'zh'));
   });
 };
 
@@ -167,19 +177,14 @@ const goToSig = (data: IObject) => {
     <o-g-anchor :data="anchorData" top="11rem"></o-g-anchor>
     <div class="wrap">
       <div class="step">
-        <span class="step-one" @click="goToTetail()">{{
-          t('nav.contributors')
-        }}</span>
+        <span class="step-one" @click="goToTetail()">{{ t('nav.contributors') }}</span>
         <span> &gt; {{ sencondTitle }}</span>
       </div>
       <div class="main">
         <div class="main-left">
           <div class="main-left-top">
             <div class="edropdown">
-              <el-dropdown
-                placement="bottom-start"
-                @visible-change="showDropdown"
-              >
+              <el-dropdown placement="bottom-start" @visible-change="showDropdown">
                 <div class="main-left-title">
                   {{ sencondTitle }}
                   <span class="btnc"></span>
@@ -199,11 +204,7 @@ const goToSig = (data: IObject) => {
                     />
                   </div>
                   <el-scrollbar ref="scrollbarRef" height="400px">
-                    <el-dropdown-item
-                      v-for="item in reallData"
-                      :key="item.value"
-                      @click="clickDrownItem(item as any)"
-                    >
+                    <el-dropdown-item v-for="item in reallData" :key="item.value" @click="clickDrownItem(item as any)">
                       {{ item }}
                     </el-dropdown-item>
                   </el-scrollbar>
@@ -218,13 +219,7 @@ const goToSig = (data: IObject) => {
             <div class="first">
               <div class="home"></div>
               <div class="toHome">
-                <a
-                  style="color: #7D32EA"
-                  target="_blank"
-                  :href="accountHome"
-                >
-                  {{ t('toHome') }}</a
-                >
+                <a style="color: #7d32ea" target="_blank" :href="accountHome"> {{ t('toHome') }}</a>
               </div>
             </div>
             <div class="firstLast">
@@ -235,32 +230,33 @@ const goToSig = (data: IObject) => {
                   <span
                     v-if="items.sig === 'TC'"
                     class="usertypecolorboxTC"
-                    :style="({
-                              '--color': '45deg, #B461F6 0%, #7D32EA 100%',
-                            } as any)"
+                    :style="
+                      {
+                        '--color': '45deg, #B461F6 0%, #7D32EA 100%',
+                      } as any
+                    "
                     >TC</span
                   >
-                  <span
-                    v-if="items.sig !== 'TC'"
-                    style="cursor: pointer"
-                    @click="goToSig(items.sig)"
-                    >{{ items.sig }}</span
-                  >
+                  <span v-if="items.sig !== 'TC'" style="cursor: pointer" @click="goToSig(items.sig)">{{ items.sig }}</span>
 
                   <span v-for="item in items.type" :key="item.value"
                     ><span
                       v-if="item === 'committers'"
                       class="usertypecolorbox"
-                      :style="({
-                              '--color': '225deg, #FEB32A 0%, #F6D365 100%',
-                            } as any)"
+                      :style="
+                        {
+                          '--color': '225deg, #FEB32A 0%, #F6D365 100%',
+                        } as any
+                      "
                       >Committer</span
                     ><span
                       v-if="item === 'maintainers' && items.sig !== 'TC'"
                       class="usertypecolorbox"
-                      :style="({
-                              '--color': '45deg, #B461F6 0%, #7D32EA 100%',
-                            } as any)"
+                      :style="
+                        {
+                          '--color': '45deg, #B461F6 0%, #7D32EA 100%',
+                        } as any
+                      "
                       >Maintainer
                     </span></span
                   >
@@ -287,11 +283,9 @@ const goToSig = (data: IObject) => {
       </div>
     </div>
     <footer>
-    <app-footer></app-footer>
-  </footer>
+      <app-footer></app-footer>
+    </footer>
   </div>
-
-
 </template>
 <style lang="scss" scoped>
 .contributors-panel {
@@ -305,7 +299,7 @@ const goToSig = (data: IObject) => {
     margin-bottom: 24px;
   }
 }
-.contributors-panel-last{
+.contributors-panel-last {
   padding: 24px;
   background: #fff;
   // margin-bottom: 24px;
@@ -325,7 +319,7 @@ const goToSig = (data: IObject) => {
   padding: 40px 0;
   font-size: 12px;
   &-one {
-    color: #7D32EA;
+    color: #7d32ea;
     cursor: pointer;
   }
   &-two {
@@ -349,7 +343,7 @@ const goToSig = (data: IObject) => {
     &-title {
       font-size: 24px;
       font-family: HarmonyOS_Sans_SC_Medium;
-      color: #7D32EA;
+      color: #7d32ea;
       line-height: 32px;
       text-overflow: ellipsis;
       width: 350px;
@@ -370,7 +364,7 @@ const goToSig = (data: IObject) => {
         position: relative;
         .toHome {
           padding-top: 3px;
-          color: #7D32EA;
+          color: #7d32ea;
           cursor: pointer;
         }
         .List {
@@ -381,7 +375,7 @@ const goToSig = (data: IObject) => {
             margin-top: 8px;
             font-size: 14px;
             font-family: HarmonyOS_Sans_SC_Medium;
-            color: #7D32EA;
+            color: #7d32ea;
             line-height: 22px;
             display: flex;
           }
@@ -404,7 +398,7 @@ const goToSig = (data: IObject) => {
         position: relative;
         .toHome {
           padding-top: 3px;
-          color: #7D32EA;
+          color: #7d32ea;
           cursor: pointer;
         }
         .List {
@@ -415,7 +409,7 @@ const goToSig = (data: IObject) => {
             margin-top: 8px;
             font-size: 14px;
             font-family: HarmonyOS_Sans_SC_Medium;
-            color: #7D32EA;
+            color: #7d32ea;
             line-height: 22px;
             display: flex;
           }
